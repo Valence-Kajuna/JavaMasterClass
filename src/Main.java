74ad11670baf95d832d0bf84e6309de0453905ee
valence <valencekajuna@yahoo.com>
2021-03-11 08:29:21 -0500

    Using constants in names Resolves #1

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,35 @@
 import java.sql.*;
 
 public class Main {
-//    public static final String DB_NAME= "testjava.db";
-//    public static final String CONNECTION_STRING = "testjava.db";
+    public static final String DB_NAME= "testjava.db";
+    public static final String CONNECTION_STRING = "jdbc:sqlite:/home/valence/IdeaProjects/TestDB/"+DB_NAME;
+    public static final String TABLE_CONTACTS = "contacts";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_PHONE = "phone";
+    public static final String COLUMN_EMAIL = "email";
 
     public static void main(String[] args) {
         try{
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:/home/valence/IdeaProjects/TestDB/testjava.db");
+            Connection conn = DriverManager.getConnection(CONNECTION_STRING);
             //conn.setAutoCommit(false);
             Statement statement = conn.createStatement();
-//            statement.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)");
-          //statement.execute("INSERT INTO contacts VALUES ('Vestina',0765975152,'valence@yahoo.com'),('Violeth',0759886654,'violeth@yahoo.com'),('Valerian',0688966410,'valerian@yahoo.com')");
-            //statement.execute("UPDATE contacts SET phone=0753932250  WHERE name ='Valerian'");
-           // statement.execute("DELETE FROM contacts WHERE name='Valerian'");
+            statement.execute("DROP TABLE IF EXISTS "+ TABLE_CONTACTS);
+            statement.execute("CREATE TABLE "+TABLE_CONTACTS +"("+COLUMN_NAME+" TEXT,"+COLUMN_PHONE+" INTEGER,"+COLUMN_EMAIL+" TEXT)");
+            statement.execute("INSERT INTO " + TABLE_CONTACTS +" VALUES ('Victor Njunwa',0753932250,'njunwawamavoko@gmail.com'),('Violeth Muganda',0759886643,'violethmukaile@ymail.com'),('Vestina Muganda', 0621876543,'vecyfreddy@gmail.com'),('Valence Kajuna',0765975152,'valencekajuna@yahoo.com')");
+            statement.execute("UPDATE " + TABLE_CONTACTS + " SET "  + COLUMN_PHONE +" = 0688966410 WHERE " + COLUMN_NAME +"='Valence Kajuna'");
+            statement.execute("DELETE FROM " + TABLE_CONTACTS + " WHERE " + COLUMN_NAME + " = 'Vestina Muganda'");
 
-//            statement.execute("SELECT * FROM contacts");
-
-            ResultSet results = statement.executeQuery("SELECT * FROM contacts");
+            ResultSet results = statement.executeQuery("SELECT * FROM "+TABLE_CONTACTS);
             while (results.next()){
-                System.out.println(results.getString("name") + " " + results.getInt("phone") + " " + results.getString("email"));
+                System.out.println(results.getString(COLUMN_NAME) + " " + results.getInt(COLUMN_PHONE) + " " + results.getString(COLUMN_EMAIL));
 
             }
-
             results.close();
             statement.close();
             conn.close();
         }catch (SQLException e){
             System.out.println("Something went wrong: "+ e.getMessage());
+            e.printStackTrace();
         }
     }
 }
